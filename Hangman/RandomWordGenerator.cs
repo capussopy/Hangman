d55ce9362ab12949d922cc9050c9fa5ecea2cc99
jan <cpaasso.jan@gmail.com>
2023-02-10 22:33:02 +0100

    Add simple random word generator

---
+++ Hangman/RandomWordGenerator.cs
@@ -0,0 +1,26 @@
+﻿namespace Hangman;
+
+public class RandomWordGenerator
+{
+    private readonly Random _random = new();
+    private readonly List<string> _words = new()
+    {
+        "COMPLAINT",
+        "INITIATIVE",
+        "PHILOSOPHY",
+        "RECOGNITION",
+        "FOUNDATION",
+        "MARKETING",
+        "COLLECTION",
+        "APARTMENT",
+        "RELATIONSHIP",
+        "INTRODUCTION"
+    };
+
+
+    public string GetNext()
+    {
+        var index = _random.Next(0, _words.Count);
+        return _words[index];
+    }
+}
---
+++ HangmanTest/RandomWordGeneratorTest.cs
@@ -0,0 +1,19 @@
+﻿using Hangman;
+
+
+namespace HangmanTest;
+
+public class RandomWordGeneratorTestShould
+{
+    [Test]
+    public void ReturnARandomWordInUpperCase()
+    {
+        var generator = new RandomWordGenerator();
+
+        var result = generator.GetNext();
+        
+        Assert.That(result, Is.InstanceOf(typeof(string)));
+        Assert.That(result, Is.EqualTo(result.ToUpper()));
+    }
+}
+
